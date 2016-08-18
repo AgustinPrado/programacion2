79ba41514602d60e46ec1779456c8bde6cb1ce95
AgustinPrado <agusprado92@gmail.com>
2016-08-17 21:46:13 -0300

    Add files via upload

---
+++ guiaDeEjercicios/Ejercicio Nro 05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_05
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numero;
+            int limite;
+            int contador1;
+            int contador2;
+            int acumulador = 0;
+            int sumaIzquierda;
+
+
+            Console.Write("\nIngrese el límite hasta el que se buscarán centros numéricos: ");
+            limite = int.Parse(Console.ReadLine());
+            for (contador1 = 1; contador1 < limite; contador1++)
+            {
+                for (contador2 = 2; contador2 < limite; contador2++)
+                {
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

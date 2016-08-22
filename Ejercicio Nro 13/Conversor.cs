a4bcd5893412ca7012dafd182a7a74819de0ad61
AgustinPrado <agusprado92@gmail.com>
2016-08-22 19:09:18 -0300

    Add files via upload

---
+++ Ejercicio Nro 13/Conversor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_13
+{
+    public class Conversor
+    {
+        public static string DecimalBinario(double numero)
+        {
+            return Convert.ToString(Convert.ToInt32(numero), 2);
+        }
+
+        public static double BinarioDecimal(string numero)
+        {
+            return Convert.ToDouble(Convert.ToInt32(numero, 2));
+        }
+    }
+}
---
+++ Ejercicio Nro 13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_13
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Ejercicio Nro 13";
+
+            Console.Write("Ingrese binario: ");
+            Console.WriteLine("Equivale en decimal a: " + Conversor.BinarioDecimal(Console.ReadLine()));
+
+            Console.Write("Ingrese decimal: ");
+            Console.WriteLine("Equivale en binario a: " + Conversor.DecimalBinario(double.Parse(Console.ReadLine())));
+
+            Console.ReadKey();
+        }
+    }
+}
